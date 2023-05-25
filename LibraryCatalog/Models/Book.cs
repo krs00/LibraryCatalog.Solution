41fbd833c19160955f7a3f30080d72d678c8b0ee
Kymani Stephens <kymanist00@gmail.com>
2023-05-25 10:26:52 -0700

    add book class

---
+++ LibraryCatalog/Models/Book.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+// using System.ComponentModel.DataAnnotations;
+
+namespace LibraryCatalog.Models
+{
+  public class Book
+  {
+    public int BookId { get; set; }
+
+  }
+}
