e7c1a1ee4e90396e43672b2aa998c1ba1e7f1d3e
Kymani Stephens <kymanist00@gmail.com>
2023-05-25 10:27:06 -0700

    add author class

---
+++ LibraryCatalog/Models/Author.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+// using System.ComponentModel.DataAnnotations;
+
+namespace LibraryCatalog.Models
+{
+  public class Author
+  {
+    public int AuthorId { get; set; }
+
+  }
+}
