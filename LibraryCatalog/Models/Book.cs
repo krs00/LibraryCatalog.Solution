569283e0ce6880b391183c36cf5dcde3f0a95134
Kymani Stephens <kymanist00@gmail.com>
2023-05-25 11:18:49 -0700

    add properties for book model

--- LibraryCatalog/Models/Book.cs
+++ LibraryCatalog/Models/Book.cs
@@ -1,11 +1,23 @@
 using System.Collections.Generic;
-// using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace LibraryCatalog.Models
 {
   public class Book
   {
     public int BookId { get; set; }
+    public string Title { get; set; } 
+    public string Genre { get; set; }
+    public string Publisher { get; set; }
+    public bool Availability { get; set; }
+
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+    [Required(ErrorMessage = "Book must have due date!")]
+    public DateTime DueDate { get; set; }
+    public List<Author> Author {  get; set; } // NAVIGATION PROPERTY  
+    public Author Author { get; set; } // REFERENCE NAVIGATION PROPERTY
+
 
   }
 }
